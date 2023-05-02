fef59ded199413dfcdc3dafe6d0db662032928fc
nrmaizatul <132249224+nrmaizatul@users.noreply.github.com>
2023-05-02 15:45:51 +0800

    Update electricity.php

--- electricity.php
+++ electricity.php
@@ -21,54 +21,47 @@
                 <input type="hour" class="form-control" id="hour" name="hour">
             </div>
             <div class="form-group">
-                <label for="rate">Current Rate</label>
-                <input type="text" class="form-control" id="rate" name="rate">
+                <label for="currentrate">Current Rate</label>
+                <input type="text" class="form-control" id="currentrate" name="currentrate">
             </div>
             <center><button type="submit" class="btn btn-primary">Calculate</button></center>
         </form>
-
+	
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $voltage = $_POST['voltage'];
   $current = $_POST['current'];
   $hour = $_POST['hour'];
-  $rate = $_POST['rate'];
+  $currentrate = $_POST['currentrate'];
   
-  function calculateElectricityCharges($voltage, $current, $hour, $rate) {
+  function calculateElectricityCharges($voltage, $current, $hour, $currentrate) {
     // Calculate power in watts
     $power = $voltage * $current;
-    
-	//calculate energy in watt-hours assuming 1 hour of usage
-	//$energy = $power * 1; 
 	
     // Calculate energy in kWh 
     $energy_kwh = $power * $hour / 1000;
     
-    // Calculate rate per hour based on current rate
-    $totalcharge = $energy_kwh * ($rate/100);
-	
-	// Calculate rate per day based on current rate
-	
+    // Calculate total charge based on current rate
+    $totalcharge = $energy_kwh * ($currentrate/100);
     
     // Return results as an array
     return array(
-        
+	
         'energy_kwh' => $energy_kwh,
         'totalcharge' => $totalcharge
     );
 }
 
 // Call the function with inputs
-$results = calculateElectricityCharges($voltage, $current, $hour, $rate);
+$results = calculateElectricityCharges($voltage, $current, $hour, $currentrate);
 
 // Print the results
-
 echo '<p> Energy:  '. $results['energy_kwh'] . ' kWh</p>';
 echo '<p> Total Charge: RM '. $results['totalcharge'].' </p>';
 }
 ?>
-
+</div>
 </body>
 </html>
 
